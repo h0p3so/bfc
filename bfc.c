b9a28b7f72b05ec869411a0e1f90e8b7b19a40f0
h0p3so <juan.dpmz05@gmail.com>
2026-03-20 19:12:00 +0000

    generating usage

---
+++ bfc.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+#include "globl.h"
+
+struct Args
+{
+	char     *in_filename;
+	char     *out_filename;
+	uint16_t arch;
+	bool     should_optimize;
+	bool     generate_source;
+};
+
+static struct Args parse_cli_arguments (const uint32_t, char**);
+static void display_usage ();
+
+int main (int argc, char **argv)
+{
+	struct Args args = parse_cli_arguments(argc, argv);
+	return 0;
+}
+
+static struct Args parse_cli_arguments (const uint32_t argc, char **argv)
+{
+	struct Args args = {0};
+
+	opterr = false;
+
+	int32_t opt;
+	while ((opt = getopt(argc, argv, "f:o:OA:hS")) != -1)
+	{
+		switch (opt)
+		{
+			case 'f': { args.in_filename = optarg;  break; }
+			case 'o': { args.out_filename = optarg; break; }
+			case 'O': { args.should_optimize = true; break; }
+			case 'S': { args.generate_source = true; break; }
+			case 'A': { args.arch = (uint16_t) atoi(optarg); break; }
+			case 'h': default : display_usage();
+		}
+	}
+
+	if ((args.arch != ARCH_64) && (args.arch != ARCH_32))
+	{
+		args.arch = ARCH_64;
+	}
+	if (args.in_filename == 0)
+	{
+		display_usage();
+	}
+
+	return args;
+}
+
+static void display_usage ()
+{
+	printf("%s usage:\n", PROGRAM_NAME);
+	printf(" $ %s -f [filename.b] [opts]\n\n", PROGRAM_NAME);
+	printf(" -f [filename] specify source file\n");
+	printf(" -o [filename] specify output's filename\n");
+	printf(" -O            optimize code (do not keep original source)\n");
+	printf(" -S            generate assembly file instead of elf file\n");
+	printf(" -A [32 | 64]  specify 32 or 64 bit arch for output\n");
+
+	exit(EXIT_SUCCESS);
+}
