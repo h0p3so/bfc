5017d7b78f0bfce54a38d1c006c5b261618f95e6
h0p3so <juan.dpmz05@gmail.com>
2026-04-30 19:12:00 +0000

    generating prelude and postlude

---
+++ common/bf-types.h
@@ -0,0 +1,13 @@
+#ifndef BFC_BF_TYPES_H
+#define BFC_BF_TYPES_H
+
+#define BF_TYPE_ADD '+'
+#define BF_TYPE_SUB '-'
+#define BF_TYPE_OUT '.'
+#define BF_TYPE_INP ','
+#define BF_TYPE_LEF '['
+#define BF_TYPE_RIG ']'
+#define BF_TYPE_NXT '>'
+#define BF_TYPE_PRV '<'
+
+#endif
---
+++ gen.h
@@ -0,0 +1,8 @@
+#ifndef BFC_GEN_H
+#define BFC_GEN_H
+
+#include "common/ir-types.h"
+
+void gen_gen (const struct IRToken*, const char*, const uint16_t);
+
+#endif
