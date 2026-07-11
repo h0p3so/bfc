820be24db1d70dadabe895757de496a717fe9d42
h0p3so <juan.dpmz05@gmail.com>
2026-07-11 16:22:17 -0500

    optimization one done

--- bfc.c
+++ bfc.c
@@ -9,29 +9,39 @@
 #include <getopt.h>
 #include <stdlib.h>
 
+#define BFC_DEFAULT_MEMORY_SIZE  30000
+#define BFC_DEFAULT_OUT_FILENAME "out.s"
+
 struct Args
 {
 	char *inFilename;
 	char *outFilename;
-	uint16_t memSize;
+	uint32_t memSize;
 	bool shouldOptmz;
 	bool shouldGenAsm;
-	bool shouldDebug;
+	bool shouldDebugLexer;
+	bool shouldDebugIR;
 };
 
 static struct Args parse_cli_arguments (const uint32_t, char**);
 static void display_usage (void);
 
-static void debug (const struct LexToken*, const char*);
+static void debug_lexer (const struct LexToken*, const char*);
+static void debug_ir (const struct IRToken*, const char*);
 
 int main (int argc, char **argv)
 {
 	struct Args args = parse_cli_arguments(argc, argv);
 
 	struct LexToken *tokens = lex_file(args.inFilename);
-	if (args.shouldDebug)
+	if (stdv_size(tokens) == 0)
 	{
-		debug(tokens, args.inFilename);
+		return 0;
+	}
+
+	if (args.shouldDebugLexer)
+	{
+		debug_lexer(tokens, args.inFilename);
 		stdv_free(tokens);
 		return 0;
 	}
@@ -39,6 +49,12 @@ int main (int argc, char **argv)
 	struct IRToken *ir = ir_gen(tokens, args.shouldOptmz);
 	stdv_free(tokens);
 
+	if (args.shouldDebugIR)
+	{
+		debug_ir(ir, args.inFilename);
+		stdv_free(ir);
+		return 0;
+	}
 	if (args.shouldGenAsm)
 	{
 		gen_gen(ir, args.outFilename, args.memSize);
@@ -55,7 +71,7 @@ static struct Args parse_cli_arguments (const uint32_t argc, char **argv)
 	opterr = false;
 
 	int32_t opt;
-	while ((opt = getopt(argc, argv, "f:o:OhSDm:")) != -1)
+	while ((opt = getopt(argc, argv, "f:o:m:OhSdD")) != -1)
 	{
 		switch (opt)
 		{
@@ -63,7 +79,8 @@ static struct Args parse_cli_arguments (const uint32_t argc, char **argv)
 			case 'o': { args.outFilename = optarg; break; }
 			case 'O': { args.shouldOptmz = true; break; }
 			case 'S': { args.shouldGenAsm = true; break; }
-			case 'D': { args.shouldDebug = true; break; }
+			case 'd': { args.shouldDebugLexer = true; break; }
+			case 'D': { args.shouldDebugIR = true; break; }
 			case 'm': { args.memSize = (uint32_t) atoi(optarg); break; }
 			case 'h': default : display_usage();
 		}
@@ -75,7 +92,11 @@ static struct Args parse_cli_arguments (const uint32_t argc, char **argv)
 	}
 	if (args.memSize == 0)
 	{
-		args.memSize = 30'000;
+		args.memSize = BFC_DEFAULT_MEMORY_SIZE;
+	}
+	if (args.outFilename == NULL)
+	{
+		args.outFilename = BFC_DEFAULT_OUT_FILENAME;
 	}
 
 	return args;
@@ -89,18 +110,40 @@ static void display_usage (void)
 	printf(" -o [filename] specify output's filename\n");
 	printf(" -O            optimize code (do not keep original source)\n");
 	printf(" -S            generate assembly file instead of elf file\n");
-	printf(" -D            print debug version\n");
+	printf(" -d            print debug version (lexer stage)\n");
+	printf(" -D            print debug version (ir stage)\n");
 	printf(" -m [usize]    specify memory size (30k default)\n");
 
 	exit(EXIT_SUCCESS);
 }
 
-static void debug (const struct LexToken *tokens, const char *filename)
+static void debug_lexer (const struct LexToken *tokens, const char *filename)
 {
-	printf("%s:debug-version:%s file:\n", PROGRAM_NAME, filename);
+	printf("%s:debug:%s file:\n", PROGRAM_NAME, filename);
 	for (uint32_t i = 0; i < (uint32_t) stdv_size(tokens); i++)
 	{
 		const struct LexToken token = stdv_get(tokens, i);
-		printf("T%5d: %5d:%-5d: %c <%5d>\n", i, token.numline, token.offset, *token.context, token.aux);
+		printf(
+			"[T%-5d (%5d:%-5d)]: <%c:%5d>\n",
+			i,
+			token.numline,
+			token.offset,
+			*token.context,
+			token.aux
+		);
+	}
+}
+
+static void debug_ir (const struct IRToken *ir, const char *filename)
+{
+	printf("%s:debug:%s file:\n", PROGRAM_NAME, filename);
+	for (uint32_t i = 0; i < (uint32_t) stdv_size(ir); i++)
+	{
+		const struct IRToken irt = stdv_get(ir, i);
+		printf(
+			"[%s: %5d]\n",
+			ir_action_as_str(irt.action),
+			irt.aux
+		);
 	}
 }
--- ir.c
+++ ir.c
@@ -2,16 +2,41 @@
 
 #include "ir.h"
 
-static struct IRToken* simple_pass (struct IRToken*, const struct LexToken*);
+#include <stdio.h>
 
-struct IRToken* ir_gen (const struct LexToken *tokens, const bool optimize)
+static struct IRToken* simple_pass (struct IRToken*, const struct LexToken*);
+static struct IRToken* optimize (struct IRToken*);
+
+struct IRToken* ir_gen (const struct LexToken *tokens, const bool shouldOptimize)
 {
 	struct IRToken *ir = stdv_create(sizeof(struct IRToken), STDV_STD_INIT_CAP);
-	if (optimize == false)
+	ir = simple_pass(ir, tokens);
+
+	if (shouldOptimize == false)
 	{
-		return simple_pass(ir, tokens);
+		return ir;
 	}
-	return ir;
+	return optimize(ir);
+}
+
+const char *ir_action_as_str (const enum IRAction action)
+{
+	switch (action)
+	{
+		case INS_NOP: return "NOP";
+		case INS_ADD: return "ADD"; 
+		case INS_SUB: return "SUB";
+		case INS_NXT: return "NXT";
+		case INS_PRV: return "PRV";
+		case INS_OUT: return "OUT";
+		case INT_INP: return "INP";
+		case INS_LEF: return "LEF";
+		case INS_RIG: return "RIG";
+		case INS_ZER: return "ZER";
+		case INS_SET: return "SET";
+	}
+
+	return NULL;
 }
 
 static struct IRToken* simple_pass (struct IRToken *ir, const struct LexToken *tokens)
@@ -25,5 +50,40 @@ static struct IRToken* simple_pass (struct IRToken *ir, const struct LexToken *t
 		};
 		stdv_put(ir, irt);
 	}
+	return ir;
+}
+
+
+static struct IRToken* optimize (struct IRToken *ir)
+{
+	struct IRToken none = {
+		.action = INS_NOP
+	};
+
+	for (uint32_t i = 0; i < (uint32_t) stdv_size(ir); i++)
+	{
+		struct IRToken *c = stdv_pget(ir, i);
+		struct IRToken *n = stdv_pget_or(ir, i + 1, none);
+
+		if ((c->action == INS_ADD && n->action == INS_SUB) || (c->action == INS_SUB && n->action == INS_ADD))
+		{
+			const int16_t aux = c->aux - n->aux;
+			if (aux == 0)
+			{
+				c->action = INS_ZER;
+				n->action = INS_NOP;
+				i += 2;
+				continue;
+			}
+
+			const bool isPositive = (aux > 0);
+			c->action = (isPositive) ? INS_ADD : INS_SUB;
+			c->aux = (uint16_t) ((isPositive) ? aux : aux * -1);
+
+			n->action = INS_NOP;
+			i += 2;
+		}
+	}
+
 	return ir;
 }
